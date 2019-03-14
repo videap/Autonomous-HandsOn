1bd21085811a400362a3727bfd527281de005a87
videap <41161153+videap@users.noreply.github.com>
2019-03-14 14:55:23 -0300

    Update 2-Criar Credenciais para Object Storage.sql

--- SQLFiles/2-Criar Credenciais para Object Storage.sql
+++ SQLFiles/2-Criar Credenciais para Object Storage.sql
@@ -5,6 +5,6 @@ begin
 DBMS_CLOUD.create_credential(
 credential_name => 'OBJ_STORE_CRED',
 username => 'Autonomous_user',
-password => 'ORl5S5LZPu48p6y(k.BV'
+password => '.K7O[wqd9.w86:3tg-sv'
 );
 end;
